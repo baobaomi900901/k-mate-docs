ab4226ca593576f6eb959d57f645744ad1c41d76
moby <baobaomi900901@icloud.com>
2026-03-20 22:04:21 +0800

    download 兼容手机端样式

--- docs/.vitepress/theme/components/Download/index.vue
+++ docs/.vitepress/theme/components/Download/index.vue
@@ -1,15 +1,10 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import { IconDownload, IconArrowBottom } from "ksw-vue-icon";
 import { i18n, useAssets } from "./i18n/index";
 import { useData } from "vitepress";
-import { KPopover  } from "ksw-ux";
-import {
-  baseUrl,
-  downloadFile,
-  findLatestVersion,
-  getUpdateLogAPI
-} from "./tools";
-import { computed, onMounted, reactive, ref } from 'vue'
+import { KPopover } from "ksw-ux";
+import { baseUrl, downloadFile, findLatestVersion, getUpdateLogAPI } from "./tools";
+import { computed, onMounted, reactive, ref } from "vue";
 import Footer from "../../components/Footer/index.vue";
 
 // 加载多语言
@@ -21,21 +16,21 @@ const windowInfo = reactive([
   {
     label: t.value.stable,
     remark: t.value.stableTip,
-    type: 'stable',
-    version: '',
+    type: "stable",
+    version: "",
   },
   {
     label: t.value.beta,
     remark: t.value.betaTip,
-    type: 'develop',
-    version: '',
+    type: "develop",
+    version: "",
   },
   {
     label: t.value.runtimeDep,
     remark: t.value.runtimeDepTip,
-    type: 'dependency'
+    type: "dependency",
   },
-])
+]);
 
 const dependencies = [
   {
@@ -50,36 +45,36 @@ const dependencies = [
     label: t.value.win7_32,
     value: 2,
   },
-]
+];
 
 const LinuxInfo = reactive([
   {
     label: t.value.stable,
     remark: t.value.stableTip,
-    type: 'stable',
-    ARMVer: '',
-    x86Ver: ''
+    type: "stable",
+    ARMVer: "",
+    x86Ver: "",
   },
   {
     label: t.value.runtimeDep,
     remark: t.value.runtimeDepTip,
-    type: 'dependency',
-    version: ''
+    type: "dependency",
+    version: "",
   },
-])
+]);
 
 const funcList = [
   {
     label: t.value.funcAuto,
     remark: t.value.funcAutoTip,
-    imgSrc: useAssets(langPrefix.value, 'editor-ui@3x.jpg')
+    imgSrc: useAssets(langPrefix.value, "editor-ui@3x.jpg"),
   },
   {
     label: t.value.funcCloud,
     remark: t.value.funcCloudTip,
-    imgSrc: useAssets(langPrefix.value, 'task-management-platform@3x.png')
+    imgSrc: useAssets(langPrefix.value, "task-management-platform@3x.png"),
   },
-]
+];
 
 const cefList = [
   {
@@ -90,89 +85,83 @@ const cefList = [
     label: t.value.arm,
     value: 2,
   },
-]
+];
 
-const plugin = ref('')
+const plugin = ref("");
 
 const initData = async () => {
   // const versionObject = await getVersionListAPI()
   const versionObject = {
-    "windows": [
+    windows: [
       {
-        "version": "2.1.0",
-        "plugin": "Resources250925"
+        version: "2.1.0",
+        plugin: "Resources250925",
       },
       {
-        "version": "2.1.1-beta16",
-        "plugin": "Resources251226"
-      }
+        version: "2.1.1-beta16",
+        plugin: "Resources251226",
+      },
     ],
-    "linux_arm": [
+    linux_arm: [
       {
-        "version": "26.02.09-beta"
-      }
+        version: "26.02.09-beta",
+      },
     ],
-    "linux_x86": [
+    linux_x86: [
       {
-        "version": "26.02.09-beta"
-      }
-    ]
-  }
+        version: "26.02.09-beta",
+      },
+    ],
+  };
   if (!versionObject) {
-    return
+    return;
   }
-  LinuxInfo[0].version = versionObject.linux_arm[0].version
+  LinuxInfo[0].version = versionObject.linux_arm[0].version;
 
   // 插件
-  const winMaxVer = findLatestVersion(versionObject.windows, 'version')
-  plugin.value = winMaxVer.plugin
+  const winMaxVer = findLatestVersion(versionObject.windows, "version");
+  plugin.value = winMaxVer.plugin;
 
   // win正式版
   const winStaMaxVer = findLatestVersion(
-    versionObject.windows.filter(item => item.version.indexOf('beta') === -1),
-    'version'
-  )
-  windowInfo[0].version = winStaMaxVer.version
+    versionObject.windows.filter((item) => item.version.indexOf("beta") === -1),
+    "version",
+  );
+  windowInfo[0].version = winStaMaxVer.version;
 
   // win开发版
   const winDevMaxVer = findLatestVersion(
-    versionObject.windows.filter(item => item.version.indexOf('beta') !== -1),
-    'version'
-  )
-  windowInfo[1].version = winDevMaxVer.version
+    versionObject.windows.filter((item) => item.version.indexOf("beta") !== -1),
+    "version",
+  );
+  windowInfo[1].version = winDevMaxVer.version;
 
   // ARM
-  const ARMMaxVer = findLatestVersion(
-    versionObject.linux_arm,
-    'version'
-  )
-  LinuxInfo[0].ARMVer = ARMMaxVer.version
+  const ARMMaxVer = findLatestVersion(versionObject.linux_arm, "version");
+  LinuxInfo[0].ARMVer = ARMMaxVer.version;
 
   // x86
-  const x86MaxVer = findLatestVersion(
-    versionObject.linux_x86,
-    'version'
-  )
-  LinuxInfo[0].x86Ver = x86MaxVer.version
-}
+  const x86MaxVer = findLatestVersion(versionObject.linux_x86, "version");
+  LinuxInfo[0].x86Ver = x86MaxVer.version;
+};
 
 const getDownloadLogoUrl = () => {
   return baseUrl + "/logo.json";
-}
+};
 
 const getNeedExtraLogoFiles = (version: string) => {
   return [
     "K-RPA Lite.zip",
     "K-RPA Lite_arm.zip",
     "K-RPA Lite_x86.zip",
-    `K-RPA Lite Setup ${version}.exe`
-  ]
-}
+    `K-RPA Lite Setup ${version}.exe`,
+  ];
+};
 
 // 处理下载逻辑（增加私有模式判断）
 const downloadWithLogoIfNeeded = async (url: string, version: string) => {
   if (!url) return;
-  const needExtraLogoFiles = getNeedExtraLogoFiles(version)
+  const needExtraLogoFiles = getNeedExtraLogoFiles(version);
   const needExtraLogo =
     import.meta.env.VP_MODE === "private" && needExtraLogoFiles.some((file) => url.endsWith(file));
 
@@ -203,53 +192,54 @@ const getDownloadRPAAndPluginUrl = (system: string, version: string) => {
       version +
       `/krpalite_${version.replaceAll("-", "_")}_amd64.deb`;
   } else {
-    fullPath =
-      baseUrl + "/" + system + "/" + version + `/K-RPA Lite Setup ${version}.exe`;
+    fullPath = baseUrl + "/" + system + "/" + version + `/K-RPA Lite Setup ${version}.exe`;
   }
   return fullPath;
 };
 
 // 下载windows
 const downloadWindow = (version: string) => {
-  const url = getDownloadRPAAndPluginUrl('windows', version)
-  downloadWithLogoIfNeeded(url, version)
-}
+  const url = getDownloadRPAAndPluginUrl("windows", version);
+  downloadWithLogoIfNeeded(url, version);
+};
 
 // 下载WebView
 const downWebView = (val: number) => {
-  let fullPath = ''
+  let fullPath = "";
   if (val === 1) {
     fullPath = baseUrl + "/webView2/x64/MicrosoftEdgeWebView2RuntimeInstallerX64.exe";
   } else if (val === 2) {
-    fullPath = 'https://download.krpalite.com:56780/webView2/win7/WebView2RuntimeInstallx64_Win7%E6%94%AF%E6%8C%81%E7%89%88.exe'
+    fullPath =
+      "https://download.krpalite.com:56780/webView2/win7/WebView2RuntimeInstallx64_Win7%E6%94%AF%E6%8C%81%E7%89%88.exe";
   } else if (val === 3) {
-    fullPath = 'https://download.krpalite.com:56780/webView2/win7/WebView2RuntimeInstallx86_Win7%E6%94%AF%E6%8C%81%E7%89%88.exe'
+    fullPath =
+      "https://download.krpalite.com:56780/webView2/win7/WebView2RuntimeInstallx86_Win7%E6%94%AF%E6%8C%81%E7%89%88.exe";
   }
-  downloadFile(fullPath)
-}
+  downloadFile(fullPath);
+};
 
 // 下载插件
 const downPlugin = () => {
   const fullPath = baseUrl + "/plugin/" + plugin.value + ".zip";
-  downloadFile(fullPath)
-}
+  downloadFile(fullPath);
+};
 
 // 下载Linux
 const downloadLinux = (system: string, version: string) => {
-  const url = getDownloadRPAAndPluginUrl(system, version)
-  downloadWithLogoIfNeeded(url, version)
-}
+  const url = getDownloadRPAAndPluginUrl(system, version);
+  downloadWithLogoIfNeeded(url, version);
+};
 
 // 下载cef
 const downCef = (val: number) => {
-  let fullPath = ''
+  let fullPath = "";
   if (val === 1) {
-    fullPath = baseUrl + "/cef/cef_x86.tar.gz"
+    fullPath = baseUrl + "/cef/cef_x86.tar.gz";
   } else {
-    fullPath = baseUrl + "/cef/cef_arm.tar.gz"
+    fullPath = baseUrl + "/cef/cef_arm.tar.gz";
   }
-  downloadFile(fullPath)
-}
+  downloadFile(fullPath);
+};
 
 /** 获取日志 */
 // const getLog = async () => {
@@ -265,47 +255,56 @@ const downCef = (val: number) => {
 // };
 
 onMounted(() => {
-  initData()
-})
+  initData();
+});
 </script>
 
 <template>
-  <div class="flex flex-col text-gray-500">
-    <section class="pt-[60px] pb-[120px] px-[80px] flex flex-col items-center bg-gradient-to-b from-blue-50 to-white">
-      <span class="font-semibold text-[#38363C] text-6xl max-lg:text-4xl">{{ t.text1 }}</span>
-      <span class="mt-2 font-medium text-lg text-[#38363C]">{{ t.text2 }}</span>
-      <!-- <span class="mt-2">{{ t.text3 }}</span> -->
-      <div class="mt-10 p-12 rounded-[40px] bg-blue-50 flex flex-col items-center w-[1440px] max-2xl:w-full">
-        <span class="text-4xl font-medium text-[#3D3D3D]">Windows {{t.client}}</span>
-        <span class="mt-2">{{t.supports}} Windows 7 / 8 / 10 /11</span>
-        <div class="w-full mt-[68px] grid gap-6 2xl:grid-cols-3 max-2xl:grid-rows-3">
+  <div class="flex flex-col text-gray-500 max-sm:w-full">
+    <section
+      class="head-bg flex h-fit flex-col items-center bg-gradient-to-b from-blue-50 to-white px-20 px-[80px] pb-[120px] pt-[60px] max-sm:px-0 max-sm:pb-6 max-sm:pt-10"
+    >
+      <span class="text-6xl font-semibold text-[#38363C] max-lg:text-4xl max-sm:text-3xl">{{
+        t.text1
+      }}</span>
+      <span class="mt-2 text-lg font-medium text-[#38363C] max-sm:text-base">{{ t.text2 }}</span>
+      <div
+        class="mt-10 flex w-[1440px] flex-col items-center rounded-[40px] bg-blue-50 p-12 max-2xl:w-full max-sm:mt-0 max-sm:w-full max-sm:py-10"
+      >
+        <span class="text-4xl font-medium text-[#3D3D3D] max-sm:text-2xl"
+          >Windows {{ t.client }}</span
+        >
+        <span class="mt-2 max-sm:mt-0 max-sm:text-base"
+          >{{ t.supports }} Windows 7 / 8 / 10 /11</span
+        >
+        <div
+          class="mt-[68px] box-content flex h-fit w-full gap-6 max-2xl:flex-col max-sm:mt-4 max-sm:gap-4 2xl:flex-row"
+        >
           <div
             v-for="(item, index) in windowInfo"
             :key="index"
-            class="bg-white rounded-[20px] flex flex-col p-5 h-[238px]"
+            class="flex min-h-56 flex-1 flex-col rounded-[20px] bg-white p-5 max-sm:min-h-48 max-sm:flex-1"
           >
-            <span class="text-[28px] text-[#111827] ">{{ item.label }}</span>
-            <span class="mt-2">{{ item.remark }}</span>
-            <span class="mt-2 text-xs" v-if="item.version">{{ t.version }}：{{ item.version }}</span>
-
-            <div v-if="item.type === 'dependency'" class="flex gap-3 mt-auto">
+            <span class="text-[28px] text-[#111827] max-sm:text-2xl">{{ item.label }}</span>
+            <span class="mt-2 max-sm:text-sm">{{ item.remark }}</span>
+            <span class="mt-2 text-xs max-sm:mt-1" v-if="item.version"
+              >{{ t.version }}：{{ item.version }}</span
+            >
+            <br />
+            <div v-if="item.type === 'dependency'" class="mt-auto flex gap-3">
               <div class="btn-down" @click="downWebView(1)">
                 WebView2
-                <IconDownload/>
-                <div class="w-[1px] h-3/5 bg-gray-300"></div>
-                <k-popover
-                  placement="bottom"
-                  :width="200"
-                  trigger="hover"
-                >
+                <IconDownload />
+                <div class="h-3/5 w-[1px] bg-gray-300"></div>
+                <k-popover placement="bottom" :width="200" trigger="hover">
                   <template #reference>
-                    <IconArrowBottom/>
+                    <IconArrowBottom />
                   </template>
                   <div class="flex flex-col">
                     <div
-                      v-for="(v,i) in dependencies"
+                      v-for="(v, i) in dependencies"
                       :key="i"
-                      class="h-[30px] px-2 flex items-center text-sm text-[#111827] cursor-pointer hover:bg-gray-100"
+                      class="flex h-[30px] cursor-pointer items-center px-2 text-sm text-[#111827] hover:bg-gray-100"
                       @click="downWebView(v.value)"
                     >
                       {{ v.label }}
@@ -313,43 +312,52 @@ onMounted(() => {
                   </div>
                 </k-popover>
               </div>
-              <div class="btn-down" @click="downPlugin()">{{t.pluginPackage}}<IconDownload/></div>
+              <div class="btn-down" @click="downPlugin()">
+                {{ t.pluginPackage }}<IconDownload />
+              </div>
             </div>
-            <div v-else class="btn-down main mt-auto" @click="downloadWindow(item.version!)">{{ t.buttonText }}<IconDownload/></div>
+            <div v-else class="btn-down main mt-auto" @click="downloadWindow(item.version!)">
+              {{ t.buttonText }}<IconDownload />
+            </div>
           </div>
         </div>
       </div>
-      <div class="mt-10 p-12 rounded-[40px] bg-red-50 flex flex-col items-center w-[1440px] max-2xl:w-full">
-        <span class="text-4xl font-medium text-[#3D3D3D]">{{ t.text4 }}</span>
-        <span class="mt-2">{{t.text5}}</span>
-        <div class="w-full mt-[68px] grid gap-6 xl:grid-cols-2 max-xl:grid-rows-2">
+      <div
+        class="mt-10 flex h-fit w-[1440px] flex-col items-center rounded-[40px] bg-red-50 p-12 max-2xl:w-full max-sm:mt-6"
+      >
+        <span class="text-4xl font-medium text-[#3D3D3D] max-sm:text-2xl">{{ t.text4 }}</span>
+        <span class="mt-2 text-center max-sm:mt-0 max-sm:text-base">{{ t.text5 }}</span>
+        <div
+          class="mt-[68px] flex h-fit w-full flex-1 gap-6 max-sm:mt-4 max-sm:flex-col max-sm:gap-4"
+        >
           <div
             v-for="(item, index) in LinuxInfo"
             :key="index"
-            class="bg-white rounded-[20px] flex flex-col p-5 h-[238px]"
+            class="flex min-h-56 flex-1 flex-col rounded-[20px] bg-white p-5 max-sm:min-h-48 max-sm:flex-1"
           >
-            <span class="text-[28px] text-[#111827]">{{ item.label }}</span>
-            <span class="mt-2">{{ item.remark }}</span>
-            <span class="mt-2 text-xs" v-if="item.version">{{t.version}}：{{ item.version }}</span>
-
-            <div v-if="item.type === 'stable'" class="flex gap-3 mt-auto">
-              <div class="btn-down" @click="downloadLinux('linux_x86', item.x86Ver!)">{{ t.x86 }}<IconDownload/></div>
-              <div class="btn-down" @click="downloadLinux('linux_arm', item.ARMVer!)">{{ t.arm }}<IconDownload/></div>
-            </div>
-            <k-popover
-              v-else
-              placement="bottom"
-              :width="200"
-              trigger="hover"
+            <span class="text-[28px] text-[#111827] max-sm:text-2xl">{{ item.label }}</span>
+            <span class="mt-2 max-sm:text-sm">{{ item.remark }}</span>
+            <span class="mt-2 text-xs max-sm:mt-1" v-if="item.version"
+              >{{ t.version }}：{{ item.version }}</span
             >
+
+            <div v-if="item.type === 'stable'" class="mt-auto flex gap-3">
+              <div class="btn-down" @click="downloadLinux('linux_x86', item.x86Ver!)">
+                {{ t.x86 }}<IconDownload />
+              </div>
+              <div class="btn-down" @click="downloadLinux('linux_arm', item.ARMVer!)">
+                {{ t.arm }}<IconDownload />
+              </div>
+            </div>
+            <k-popover v-else placement="bottom" :width="200" trigger="hover">
               <template #reference>
-                <div class="btn-down mt-auto">CEF {{t.framework}}<IconArrowBottom/></div>
+                <div class="btn-down mt-auto">CEF {{ t.framework }}<IconArrowBottom /></div>
               </template>
               <div class="flex flex-col">
                 <div
-                  v-for="(v,i) in cefList"
+                  v-for="(v, i) in cefList"
                   :key="i"
-                  class="h-[30px] px-2 flex items-center text-sm text-[#111827] cursor-pointer hover:bg-gray-100"
+                  class="flex h-[30px] cursor-pointer items-center px-2 text-sm text-[#111827] hover:bg-gray-100"
                   @click="downCef(v.value)"
                 >
                   {{ v.label }}
@@ -360,17 +368,29 @@ onMounted(() => {
         </div>
       </div>
     </section>
-    <section class="py-[120px] px-[80px] bg-[#FCF5EB] flex flex-col items-center">
-      <span class="text-[#000000] font-semibold text-[56px] max-lg:text-4xl">{{t.text6}}<b class="text-[#A52FFF]">{{t.text7}}</b></span>
-      <div class="mt-[60px] grid gap-6 w-[1440px] max-2xl:w-full xl:grid-cols-2 max-xl:grid-rows-2">
+    <section
+      class="flex flex-col items-center bg-[#FCF5EB] px-[80px] py-[120px] max-sm:px-10 max-sm:py-10"
+    >
+      <span
+        class="text-center text-[56px] font-semibold text-[#000000] max-lg:text-4xl max-sm:px-12 max-sm:text-2xl"
+        >{{ t.text6 }}<b class="text-[#A52FFF]">{{ t.text7 }}</b></span
+      >
+      <div
+        class="mt-[60px] flex w-[1440px] gap-6 max-2xl:w-full max-xl:grid-rows-2 max-sm:mt-4 max-sm:flex-col"
+      >
         <div
-          v-for="(item, index) in funcList" :key="index"
-          class="rounded-[20px] bg-white flex flex-col overflow-hidden h-[600px]"
+          v-for="(item, index) in funcList"
+          :key="index"
+          class="flex h-[600px] flex-col overflow-hidden rounded-[20px] bg-white max-sm:h-[480px]"
         >
-          <span class="text-[40px] text-[#000000] font-semibold px-12 mt-12">{{ item.label }}</span>
-          <span class="mt-4 text-lg px-12">{{ item.remark }}</span>
-          <div class="bg-gradient-to-r from-[#f6e1ff] to-[#fff4f5] flex-1 mt-6 ml-12 rounded-tl-[20px] overflow-hidden">
-            <img :src="item.imgSrc" class="w-[660px] h-[511px] object-cover object-left-top">
+          <span class="mt-12 px-12 text-[40px] font-semibold text-[#000000] max-sm:text-2xl">{{
+            item.label
+          }}</span>
+          <span class="mt-4 px-12 text-lg max-sm:mt-1 max-sm:text-base">{{ item.remark }}</span>
+          <div
+            class="ml-12 mt-6 flex-1 overflow-hidden rounded-tl-[20px] bg-gradient-to-r from-[#f6e1ff] to-[#fff4f5]"
+          >
+            <img :src="item.imgSrc" class="h-[511px] w-[660px] object-cover object-left-top" />
           </div>
         </div>
       </div>
@@ -380,13 +400,18 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
+.head-bg {
+  background-image: url("../../assets/download-bg.png");
+  background-repeat: no-repeat;
+  background-position: center -280px;
+}
 .btn-down {
   width: fit-content;
   display: flex;
   align-items: center;
   gap: 8px;
   padding: 12px 28px;
-  border: 1px solid #9CA3AF;
+  border: 1px solid #9ca3af;
   border-radius: 100px;
   font-size: 14px;
   color: #111827;
@@ -396,12 +421,21 @@ onMounted(() => {
 
   &.main {
     color: #fff;
-    border: 1px solid #3B82F6;
-    background-color: #3B82F6;
+    border: 1px solid #3b82f6;
+    background-color: #3b82f6;
   }
 
   &:hover {
     opacity: (0.8);
+  }
+}
+
+@media not all and (min-width: 640px) {
+  .btn-down {
+    padding: 0.375rem 0.75rem;
+    font-size: 0.815rem;
+    display: flex;
+    gap: 0.25rem;
   }
 }
 </style>
